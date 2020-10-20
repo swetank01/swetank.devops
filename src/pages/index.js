8380a9626b6f9d7567080303ce223d3a184098ff
swetank soni <iamswetanksoni@gmail.com>
2020-10-20 17:50:55 +0530

    init 6 : gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>Swetank Soni</h1> 
+    <h2>Devops Engineer</h2>
+  </div>
 }
