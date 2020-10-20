c4dccd6dffe06a19e2c37e90a8d46ae862a42518
swetank soni <iamswetanksoni@gmail.com>
2020-10-20 17:51:26 +0530

    add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const contact = () => {
+    return(
+        <div>
+            <h1>Contact Me</h1>
+            <h2>Email me swetankdevops@gmail.com </h2>
+            <div>
+                <ul>
+                    <li>LinkedIn</li>
+                    <li>Github</li>
+                    <li>Behance</li>
+                    <li>Unsplash</li>
+                    <li>Twitter</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default contact
